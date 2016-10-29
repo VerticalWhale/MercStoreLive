8c3ac0857e71710469d4ec5d005fac7970fd4c4a
Ethan <ethan@equityfirstinc.com>
2016-10-29 16:02:00 -0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -6,35 +6,35 @@
 exports.view = function(req, res){
   res.render('index', {
   	'projects': [
-      { 'name': 'Waiting in Line',
+      { 'name': 'LMAO DEM JUKES',
         'image': 'lorempixel.people.1.jpeg',
         'id': 'project1'
       },
-      { 'name': 'Needfinding',
+      { 'name': 'YEEEEEEEEEEEEEEEEEEEEEEEEEEEET',
         'image': 'lorempixel.city.1.jpeg',
         'id': 'project2'
       },
-      { 'name': 'Prototyping',
+      { 'name': 'Wow guy',
         'image': 'lorempixel.technics.1.jpeg',
         'id': 'project3'
       },
-      { 'name': 'Heuristic Evaluation',
+      { 'name': 'Dat Boi',
         'image': 'lorempixel.abstract.1.jpeg',
         'id': 'project4'
       },
-      { 'name': 'Visualization',
+      { 'name': 'Yo pass the Aux cord', 
         'image': 'lorempixel.abstract.8.jpeg',
         'id': 'project5'
       },
-      { 'name': 'Social design',
+      { 'name': 'You better not play trash',
         'image': 'lorempixel.people.2.jpeg',
         'id': 'project6'
       },
-      { 'name': 'Gestural interaction',
+      { 'name': 'Pokemon go song',
         'image': 'lorempixel.technics.2.jpeg',
         'id': 'project7'
       },
-      { 'name': 'Design tools',
+      { 'name': 'Heavy Whip',
         'image': 'lorempixel.city.2.jpeg',
         'id': 'project8'
       }
